ca868b26fb278b80eea4387d592a46abfa5896a4
Cruellest <gabriel81523642@gmail.com>
2024-06-04 20:25:05 -0400

    Header and Bootstrap added

--- Grupo-10_PW-T01-2024-1/src/App.jsx
+++ Grupo-10_PW-T01-2024-1/src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Header from "./components/Header"
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
  
 function App() {
     return (
