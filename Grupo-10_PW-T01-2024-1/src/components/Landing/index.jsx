84312293a7c35728b59224e2932622f2380445fd
Maria Fernanda Colombo <140662320+mafecolombo@users.noreply.github.com>
2024-06-06 19:30:42 -0300

    Updating Header and Landing Page

---
+++ Grupo-10_PW-T01-2024-1/src/components/Landing/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div class="container-fluid" id="landing">
+        <div> oi</div>
+    </div>
+  );
+}
+
+export default Landing;
