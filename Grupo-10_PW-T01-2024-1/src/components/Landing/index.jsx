a7b334db8a3100b9f6c70e5914e762acf9b8f5cc
Maria Fernanda Colombo <140662320+mafecolombo@users.noreply.github.com>
2024-06-06 21:15:06 -0300

    Ending Home Page

--- Grupo-10_PW-T01-2024-1/src/components/Landing/index.jsx
+++ Grupo-10_PW-T01-2024-1/src/components/Landing/index.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 function Landing() {
   return (
     <div class="container-fluid" id="landing">
-        <div> oi</div>
+        <div class="container text-center" id="landing-content">
+            <img id="tittle" src='../src/assets/tittle.png'/>  
+            <div class="row" id="row-landing">
+                <div class="container text-center">
+                    Which dish deserves to win this battle?
+                </div>
+            </div>
+            <div class="row" id="row-landing">
+                <div class="container text-center">
+                <button type="button" class="btn btn-warning" style={{ fontWeight: 'bolder' }}>Start</button>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
