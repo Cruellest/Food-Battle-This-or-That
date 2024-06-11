a14da71aadc6081ff383f8efbfcd29683ff45e0c
Cruellest <gabriel81523642@gmail.com>
2024-06-11 18:52:25 -0400

    Made some fixes

--- Grupo-10_PW-T01-2024-1/src/components/Header/index.jsx
+++ Grupo-10_PW-T01-2024-1/src/components/Header/index.jsx
@@ -8,22 +8,22 @@ const Header = () => {
     <header id="header">
         <div>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid" id="nav-background">
+                <div class="container-fluid " id="nav-background">
                     <a class="navbar-brand" href="#"><img class="navbar-brand" src='../src/assets/logo.svg'/></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span><i class="bi bi-list"></i></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                        <button class="btn btn-outline-light" type="submit">Search</button>
+                    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                    <form class="d-flex justify-content-center " role="search">
+                        <input class="form-control me-2 justify-content-center" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button class="btn btn-outline-light align-center" type="submit">Search</button>
                     </form>
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">home</a>
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">categories</a>
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categories</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="#">Action</a></li>
                             <li><a class="dropdown-item" href="#">Another action</a></li>
@@ -33,7 +33,7 @@ const Header = () => {
                         </li>
                         <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            ranking
+                            Ranking
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="#">Action</a></li>
@@ -43,7 +43,7 @@ const Header = () => {
                         </ul>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link active" href="#" aria-current="page">sign in/login</a>
+                        <a class="nav-link active" href="#" aria-current="page">Sign in/Login</a>
                         </li>
                     </ul>
                     </div>
