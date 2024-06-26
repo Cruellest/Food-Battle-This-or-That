9a1bfbcfe7f04c461cfe28abc7b7b300bf177274
Cruellest <gabriel81523642@gmail.com>
2024-06-26 02:48:48 -0400

    Footer linking to linkedIn

--- Grupo-10_PW-T01-2024-1/src/components/Footer/index.jsx
+++ Grupo-10_PW-T01-2024-1/src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
 return (
     <div className="container text-center">    
-        <footer>     <p>&copy; 2024 Colombo&Baltazar.</p>     </footer>    
+        <footer>     <p>&copy; 2024 <a href='https://www.linkedin.com/in/maria-fernanda-colombo-533436276/'>Colombo</a>&<a href='https://www.linkedin.com/in/cruellest'>Baltazar</a>.</p>     </footer>    
     </div>
       );
 }
