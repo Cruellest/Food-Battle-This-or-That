29c67e79e95a883912ea0b1c089ba8b959506323
Cruellest <gabriel81523642@gmail.com>
2024-07-06 03:46:59 -0400

    Keys removed

--- Grupo-10_PW-T01-2024-1/src/services/firebaseConnection.jsx
+++ Grupo-10_PW-T01-2024-1/src/services/firebaseConnection.jsx
@@ -5,13 +5,9 @@ import { getFirestore, doc, getDoc, setDoc, updateDoc, increment } from 'firebas
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoCdhiMEVk181A39UfsdQLNarS2p66i4U",
-  authDomain: "food-battle-this-or-that.firebaseapp.com",
-  projectId: "food-battle-this-or-that",
-  storageBucket: "food-battle-this-or-that.appspot.com",
-  messagingSenderId: "879528474867",
-  appId: "1:879528474867:web:dcde30c31f426775e39f86",
-  measurementId: "G-N6PGDZH973"
+
+  //Add your keys here
+
 };
 
 // Initialize Firebase
